dc7c3446dfdbf4ff2753e68fa68142ecbd77206c
Larsern1991 <lars.kristian.maehlum@hotmail.com>
2018-02-07 11:12:56 +0100

    added some comments

--- krack-all-zero.py
+++ krack-all-zero.py
@@ -503,6 +503,7 @@ class KRAckAttack():
 		ps = sniff(count=1, timeout=0.3, lfilter=lambda p: Dot11Beacon in p and get_tlv_value(p, IEEE_TLV_TYPE_SSID) == ssid, opened_socket=self.sock_real)
 		if ps is None or len(ps) < 1:
 			log(STATUS, "Searching for target network on other channels")
+			#Går gjennom og sniffer i hver av kanalene
 			for chan in [1, 6, 11, 3, 8, 2, 7, 4, 10, 5, 9, 12, 13]:
 				self.sock_real.set_channel(chan)
 				log(DEBUG, "Listening on channel %d" % chan)
