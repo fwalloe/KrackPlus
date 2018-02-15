00ffe376d26479df0172c7810844c37a3caf8afa
root <root@localhost.localdomain>
2018-02-15 13:22:52 -0500

    Added a TODO in prerequisites.sh

--- prerequisites.sh
+++ prerequisites.sh
@@ -3,3 +3,15 @@ apt-get install libnl-3-dev libnl-genl-3-dev pkg-config libssl-dev net-tools git
 ./krackattacks-scripts/krackattack/disable-hwcrypto.sh
 nmcli radio wifi off
 sudo rfkill unblock wifi
+
+# i /etc/wpa_supplicant må det sjekkes om det finnes en wpa_supplicant.conf fil, hvis denne ikke finnes så må den lages.
+# i wpa_supplicant.conf så må det stå:
+# ctrl_interface=/var/run/wpa_supplicant
+#      network={
+#         ssid="Brennbakkvegen194"
+#          key_mgmt=FT-PSK
+#         psk="Sonic2016"
+#      }
+
+
+
