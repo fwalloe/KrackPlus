88ce312716c3883ceb2293d6a52ce97c018fa388
Fredrik Walloe <code@fredrikwalloe.com>
2018-02-15 18:19:38 +0100

    Added some basic text to the GUI

--- krackPlus.py
+++ krackPlus.py
@@ -1,17 +1,20 @@
 #!/bin/python
 
-#Kivy framework is used for GUI
+# Kivy framework is used for GUI
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
+from kivy.uix.label import Label 
 
 
+# Defines the instance of the widget.
 class KrackWidget(Widget):
-	pass
+	pass # pass is used to keep the class valid but allow it not to contain anything 
 
 class KrackPlusApp(App):
-    def build(self):
-        return KrackWidget()
+	def build(self):
+		exampleLabel=Label(text='Welcome to KRACK+')
+		return exampleLabel
 
 
 if __name__ == '__main__':
