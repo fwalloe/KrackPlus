0efadbb6a7a687ea09a356cf8b7fb2afdaedd784
Fredrik Walloe <code@fredrikwalloe.com>
2018-02-16 23:22:58 +0100

    We now have a button. Click it to check whether a device is vulnerable

--- krackPlus.py
+++ krackPlus.py
@@ -6,6 +6,8 @@
 # Development notes:
 ## all of this is experimental. Feel free to change everything 
 
+### IMPORTS ###
+
 # Kivy framework is used for GUI
 from kivy.app import App
 from kivy.uix.widget import Widget
@@ -15,39 +17,32 @@ from kivy.core.window import Window
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
 
-## TEMPORARY
-red = [1,0,0,1]
-green = [0,1,0,1]
-
-
 # Subprocess is used to run bash commands
 import subprocess
-
-
 
 # Defines the instance of the widget.
 class KrackWidget(Widget):
 	pass # used to keep a class valid even though it doesn't contain anything
 
-# Sets up 
+# 
 def callback(instance):
-	print('Processing... ' % instance.text)
-	### NOTE move this out of the class to set scan for vulnerable clients whenever the program starts.
-#	subprocess.call("./prepareClientScan.sh")
-
+	print('Processing...')
+	subprocess.call("./prepareClientScan.sh")
 
 # Setup: installs necessary software and sets up a test network 
 class ClientScanSetup(Widget):	
-	btn1 = Button(text='Check whether device is vulnerable')
-	btn1.bind(on_press=callback)	
+	vulnCheckButton = Button(text='Check whether device is vulnerable')
+	vulnCheckButton.bind(on_press=callback)	
 
 class KrackPlusApp(App):
 	def build(self):
 		Window.size = (820, 580)	
 		#TODO should be a nice gray or something
 		Window.clearcolor = (.12,.12,.12,.12)
-		
-		return ClientScanSetup()
+		#TODO btn1 should be removed eventually
+		btn1 = Button(text='Click to check whether device is vulnerable')
+		btn1.bind(on_press=callback)	
+		return btn1
 
 				
 
