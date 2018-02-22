91f31074ee40ed189e90f81ada9052a7ceefab64
Lars Kristian <lars.kristian.maehlum@hotmail.com>
2018-02-21 22:42:44 -0600

    Manipulates the output and generates a report

---
+++ outputHandler.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+input=$1
+mac=""
+IP=""
+macIP=""
+NMAP=$2
+
+echo "Report"
+echo " "
+
+#Unique elements only
+macAndIP=$(cat $input | grep "DHCP reply" | uniq)
+
+#Make newline the only separator in the loop
+IFS=$'\n'
+
+for line in $macAndIP; do
+    addMac=$(echo $line | awk '{print $7}' | cut -b 1-17)
+    addIP=$(echo $line | awk '{print $5}')
+    mac="$mac$addMac\n"
+    IP="$IP$addIP\n"
+    macIP="$macIP$addMac $addIP\n"
+done
+
+printf "Mac-addresses:\n$mac"
+
+printf "IP-adresses:\n$IP"
+
+#Reuse the mac-parameter
+mac=""
+
+#The mac-addresses vulnerable against pairwise key reinst.
+pairwiseVuln=$(cat $input | grep "Client is vulnerable to pairwise" | uniq)
+
+if [[ $(echo $pairwiseVuln | wc -l) -gt 0 ]]; then
+    echo "Clients vulnerable to pairwise key reinstallations in the 4-way handshake:"
+    #Extract the mac-addresses from the output
+    for line in $pairwiseVuln; do
+        mac="$mac$(echo $line | awk '{print $2}' | cut -b 1-24)\n"
+    done
+    printf $mac
+    else
+        echo "No clients vulnerable to pairwise key reinstallations in the 4-way handshake"
+fi
+
+#The mac-addresses vulnerable against group key reinst.
+groupVuln=$(cat $input | grep "Client is vulnerable to group" | uniq)
+if [[ $(echo $groupVuln | wc -l) -gt 0 ]]; then
+    echo "Clients vulnerable to group key reinstallations in the 4-way handshake:"
+    #Extract the mac-addresses from the output
+    for line in $groupVuln; do
+        mac="$mac$(echo $line | awk '{print $2}' | cut -b 1-24)\n"
+        printf $line | awk '{print $2}' | cut -b 1-24
+    done
+
+    else
+    echo "No clients vulnerable to group key reinstallations in the 4-way handshake"
+
+fi
+
+#Discard duplicate results
+mac=$(printf $mac | uniq)
+
+#Find the IP-addresses belonging to the mac-addresses
+for line in $mac; do
+    if [[ $macIP = *":"* ]]; then
+        #nmap -O $(printf $macIP | grep $line | awk '{print $2}')
+        printf $macIP | grep -E *$line* # | awk '{print $2}'
+        #printf $line
+    fi
+done
+
+#TODO: Kjøre n-map mot vulnerable clients
