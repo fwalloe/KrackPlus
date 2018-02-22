49b6a75e301a0775efd652ade3bc79d91d54ae50
Lars Magnus Trinborgholen <lmtrinbo@stud.ntnu.no>
2018-02-21 22:05:15 -0500

    Answered some TODO questions, and added some TODO's. Also added some comments.

--- krackPlusCLI.py
+++ krackPlusCLI.py
@@ -6,19 +6,19 @@ import subprocess
 def main():
     parse = optparse.OptionParser();
     
-    ## TODO er det mulig å gjøre en slik type scan? I sciptet vi kjørte måtte vi sette opp et nettverk som man så måtte logge seg på. 
 
-    #Adding option to run scan
+    #Adding option to run scan.                                                             TODO Task 2: Make scan of vulnerability work
     parse.add_option('--scan', '-s', default=False, help="This option will run a vulnerability scan against the given IP")
 
-    #Adding option to run attack against .....
+    #Adding option to run attack against .....                                              TODO Senere, one thing at a time.
     parse.add_option('--attack', '-a', default=False, help="This option will run a key reinstallation attack against the given IP")
     
-    #Adding option to install dependencies and turn of hardware encryption on NIC....etc.
+    #Adding option to install dependencies and turn of hardware encryption on NIC....etc.   TODO Task 1: Make dependencies script work, and make sure all is good after doing this option.
     parse.add_option('--prepare', '-p', default=False, help="This option will prepare for scans and attacks. Takes 'scan', 'attack' for paramters for now.")
     
-    ## TODO savner en kommentar her. Hva skjer her? Er det slik at vi sjekker om man allerede har satt opp requirements? I så fall vil ikke dette lagres mellom ver kjøring av programmet, slik det er satt opp nå. 
     options, args = parse.parse_args()
+
+    # These bools are to be set to true, if client is already prepared.                     TODO Make a script to check.
     isClientPreparedScan = False
     isClientPreparedAttack = False
     
@@ -35,25 +35,23 @@ def main():
     elif options.prepare == 'attack':
         if isClientPreparedAttack == False:
             print("Preparing client for " + options.prepare + " ...")
-	    #TODO dette scriptet finnes ikke. Har det bare ikke blitt pushet, eller?
+#                                                                                           TODO Create this script.
             subprocess.call(["prepareClientAttack.sh"])
             isClientPreparedAttack = True
         else:
             print("Already prepared for" + options.prepare + ". Please continue..")
-            
-    ## TODO hvilket scan script? Vi kan med fordel ha mer utdypende kommentarer
+                                                                                  
     # Running scan scripts
     if options.scan != False:
         if isClientPreparedScan:
             print("Scanning" + options.scan + ":")
-            subprocess.call(["vulnerabilityScan.sh"]) #TODO usikker paa filnavnet her, kodet i bitbucket av alle ting.
+            subprocess.call(["vulnerabilityScan.sh"])  #                    TODO Wrong filename, but this script exists
     
     # Running attack scripts
     elif options.attack != False:
         if isClientPreparedAttack:
             print("Performing key reinstallation attack against " + options.attack)
-            #TODO her var det en halvferdig linje. Kommentert ut. 
-            #subprocess.call(["
+            #TODO subprocess, run attack script.
         
 if __name__ == '__main__':
     main()
