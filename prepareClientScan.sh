32f173c4fbdb1a733cac17f4930b1244f7715062
Fredrik Walløe <root@localhost.localdomain>
2018-03-07 14:45:34 +0100

    Users can now set a custom ssid and password

--- prepareClientScan.sh
+++ prepareClientScan.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-#### NOTE: Only install dependencies if not already installed. See second answer here for how to do that: 
-# https://stackoverflow.com/questions/1298066/check-if-a-package-is-installed-and-then-install-it-if-its-not
-
-# Install dependencies
+# Installs dependencies
 echo "Setting up dependencies..."
 
-#Checks whether dependencies are already installed; if not, installs them.
+# Checks whether dependencies are already installed; if not, installs them.
 ## NOTE / TODO maybe need to use gksudo with GUI
 while read packages; do
 	PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $packages | grep "install ok installed")
@@ -30,32 +27,17 @@ then
 	cd ../../
 fi
 
-#Disable network
+# Disables network
 nmcli radio wifi off
 
-# Disable hardware encryption, as bugs on some Wi-Fi network interface cards could interfere with the script used to check whether a client is vulnerable
-## NOTE we should also make sure that this is reversed when the user is done... Perhaps make it an option
+# Disables hardware encryption, as bugs on some Wi-Fi network interface cards could interfere with the script used to check whether a client is vulnerable
+## TODO we should also make sure that this is reversed when the user is done... Perhaps make it an option
 ./findVulnerable/krackattack/disable-hwcrypto.sh
 
-#Let user choose whether to reboot computer
-## NOTE not implemented
-
-#### NOTE: next sections not done
-
-#RUN: systool -vm ath9k_htc
-
-## To check: the nohwcript/.. param has been set.
-
-#Look for key reinstallations in the 4-way handshake
-./findVulnerable/krackattack/krack-test-client.py
-
-#Look for key reinstallations in the group key handshake 
-#      ./krack-test-client.py --group
-
-
-# Default network name:testnetwork 
-# Default password: abcdefgh
-
-### NOTE.: let user change this; must modify hostapd.conf
-# You will probably have to edit the line `interface=` to specify the correct Wi-Fi interface to use for the AP.
-
+# Replace default password if user requests it
+echo $1
+if [ $1 == "customCredentials" ]
+then
+	sed -i "88s/.*/ssid=$(sed '1q;d' networkCredentials.txt)/" ./findVulnerable/hostapd/hostapd.conf
+	sed -i "1146s/.*/wpa_passphrase=$(sed '2q;d' networkCredentials.txt)/" ./findVulnerable/hostapd/hostapd.conf
+fi 
