730d01efda3184f326a68b1c5d2a8a2de39dc710
Lars Kristian <lars.kristian.maehlum@hotmail.com>
2018-03-08 08:20:10 -0600

    New not-working version

--- outputHandler.sh
+++ outputHandler.sh
@@ -1,70 +1,72 @@
 #!/bin/bash
 
 input=$1
-mac=""
-IP=""
-macIP=""
-NMAP=$2
+fileContent=""
+newFileContent=""
+diff=""
 
 reportName='reportTemplate.tex'
 reportPath='./'
 
-# $1 is the output-string and $2 is the line number
+#Make newline the only separator in the loop
+#IFS=$'\n'
 
+# $1 is the output-string and $2 is the line number
 writePDF() {
 sed -i "$2s/.*/$1/" $reportPath$reportName
-echo test
 }
 
-echo "Report"
-echo " "
-
-#Unique elements only
-macAndIP=$(cat $input | grep "DHCP reply" | uniq)
+# $1 is the new file content
+checkOutput() {
 
 #Make newline the only separator in the loop
 IFS=$'\n'
 
+output="$1"
+
+#Unique elements only
+macAndIP=$(printf "$output" | grep "DHCP reply" | uniq)
+printf "MAcANDIP: $macAndIP"
+#printf "file::: $output"
+
 for line in $macAndIP; do
-    #addMac=$(echo $line | awk '{print $7}' | cut -b 1-17)
-    addMac=$(echo $line | cut -b 25- | grep -Eo '*([0-9a-f]{2}\:){5}[0-9a-f]{2}')
-    #addIP=$(echo $line | awk '{print $5}')
+    addMac=$(echo $line | grep -Eo '*([0-9a-f]{2}\:){5}[0-9a-f]{2}')
     addIP=$(echo $line | grep -Eo '*([0-9]{1,3}\.){3}[0-9]{1,3}')
+
     mac="$mac$addMac\n"
     IP="$IP$addIP\n"
+
     macIP="$macIP$addMac $addIP\n"
+    macIP=$(printf $macIP | uniq)
 done
 
-printf "IP-adresses that has been scanned:\n$IP\n"
-
-#Reuse the mac-parameter
-mac=""
-
 #The mac-addresses vulnerable against pairwise key reinst.
-pairwiseVuln=$(cat $input | grep "Client is vulnerable to pairwise" | uniq)
+pairwiseVuln=$(printf "$output" | grep "Client is vulnerable to pairwise" | uniq)
+
+vulnMac=""
 
 if [[ $(echo $pairwiseVuln | wc -l) -gt 0 ]]; then
     echo "Clients vulnerable to pairwise key reinstallations in the 4-way handshake:"
     #Extract the mac-addresses from the output
     for line in $pairwiseVuln; do
          #mac="$mac$(printf $line | awk '{print $2}' | cut -b 8-24)\n"
-       mac="$mac$(printf $line | grep -Eo '*([0-9a-f]{2}\:){5}[0-9a-f]{2}')\n"
+       vulnMac="$vulnMac$(echo $line | grep -Eo '*([0-9a-f]{2}\:){5}[0-9a-f]{2}')\n"
     done
-    printf "$mac\n"
-    writePDF $mac 43
+    printf "$vulnMac\n"
+    #writePDF $vulnMmac 43
 
     else
         echo "No clients vulnerable to pairwise key reinstallations in the 4-way handshake"
 fi
 
 #The mac-addresses vulnerable against group key reinst.
-groupVuln=$(cat $input | grep "Client is vulnerable to group" | uniq)
+groupVuln=$(printf "$output" | grep "Client is vulnerable to group" | uniq)
 if [[ $(echo $groupVuln | wc -l) -gt 0 ]]; then
     echo "Clients vulnerable to group key reinstallations in the 4-way handshake:"
     #Extract the mac-addresses from the output
     for line in $groupVuln; do
         #mac="$mac$(printf $line | awk '{print $2}' | cut -b 8-24)\n"
-        mac="$mac$(printf $line | grep -Eo '*([0-9a-f]{2}\:){5}[0-9a-f]{2}')\n"
+        vulnMac="$vulnMac$(echo $line | grep -Eo '*([0-9a-f]{2}\:){5}[0-9a-f]{2}')\n"
         printf $line | grep -Eo '*([0-9a-f]{2}\:){5}[0-9a-f]{2}'
     done
     else
@@ -73,14 +75,26 @@ fi
 echo ""
 
 #Discard duplicate results
-mac=$(printf $mac | uniq)
+#vulnMac=$(printf $vulnMac | uniq)
 
 #Find the IP-addresses belonging to the mac-addresses
 
 echo "IP-address of vulnerable mac-addresses:"
-
-for line in $mac ; do
+echo "MACIP: $macIP end"
+for line in $vulnMac ; do
    if [[ "$macIP" == *"$line"* ]] ; then
         printf $macIP | grep $line | grep -Eo '*([0-9]{1,3}\.){3}[0-9]{1,3}'
    fi
 done
+
+}
+
+echo "Report"
+echo " "
+
+
+newFileContent=$(cat "$input")
+diff=$(printf "$newFileContent" | grep -v "$fileContent")
+fileContent="$newFileContent"
+
+checkOutput "$newFileContent"
