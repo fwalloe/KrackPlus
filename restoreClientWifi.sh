b798724b460aa4eb5ef6789d00eaab4ef87a9e70
Fredrik Walloe <code@fredrikwalloe.com>
2018-03-13 14:27:41 +0100

    Added line that starts NetworkManager

--- restoreClientWifi.sh
+++ restoreClientWifi.sh
@@ -4,6 +4,10 @@
 
 wlanName=$(iw dev | awk '/Interface/ {print $2}')
 
+systemctl start NetworkManager
+
 ifconfig $wlanName down
 iwconfig $wlanName mode managed
 ifconfig $wlanName up
+
+# TODO check whether the wlan interfaces are back up; if not, advice user to remove their external NIC and click a button to run this script again. 
