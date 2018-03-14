0007f1a9f7e4f84963f589a00d651058715eb8b2
Fredrik Walløe <root@localhost.localdomain>
2018-03-14 14:55:50 +0100

    Began to improve this, but did not finish. Code commented out for now

--- restoreClientWifi.sh
+++ restoreClientWifi.sh
@@ -6,10 +6,12 @@ nmcli radio wifi on
 
 systemctl start NetworkManager
 
-wlanName=$(ifconfig -a | sed 's/[ \t].*//;/^$/d' | awk 'FNR==3' | tr -d ':')
-
-ifconfig $wlanName down
-iwconfig $wlanName mode managed
-ifconfig $wlanName up
-
+#for i in {1..3}
+#do
+	wlanName=$(ifconfig -a | sed 's/[ \t].*//;/^$/d' | awk "FNR==$i" | tr -d ':')
+	echo "hello"
+	ifconfig $wlanName down
+	iwconfig $wlanName mode managed
+	ifconfig $wlanName up
+#done
 # TODO check whether the wlan interfaces are back up; if not, advice user to remove their external NIC and click a button to run this script again. 
