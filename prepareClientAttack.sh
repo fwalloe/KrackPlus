e287f3e8def4ad72ed1c6989a31a8ae8ac5e9235
Fredrik Walløe <root@localhost.localdomain>
2018-03-20 13:40:00 +0100

    Commented out some work in progress

--- prepareClientAttack.sh
+++ prepareClientAttack.sh
@@ -4,21 +4,21 @@
 echo "Setting up dependencies..."
 
 # Set interface variables:
-eth0=$(ifconfig -a | sed 's/[ \t].*//;/^$/d' | awk 'FNR==1' | tr -d ':')
+#eth0=$(ifconfig -a | sed 's/[ \t].*//;/^$/d' | awk 'FNR==1' | tr -d ':')
 
-wlan0=$(ifconfig -a | sed 's/[ \t].*//;/^$/d' | awk 'FNR==3' | tr -d ':')
+#wlan0=$(ifconfig -a | sed 's/[ \t].*//;/^$/d' | awk 'FNR==3' | tr -d ':')
 
-wlan1=$(ifconfig -a | sed 's/[ \t].*//;/^$/d' | awk 'FNR==4' | tr -d ':')
+#wlan1=$(ifconfig -a | sed 's/[ \t].*//;/^$/d' | awk 'FNR==4' | tr -d ':')
 
 # Replace hard-coded interface value in dnsmasq.conf
-sed -i "1s/.*/interface=$(sed '2q;d' $wlan1)/" ./krackattacks-poc-zerokey/krackattack/dnsmasq.conf
+#sed -i 1s/.*/interface=$(sed '2q;d' $wlan1)/ ./krackattacks-poc-zerokey/krackattack/dnsmasq.conf
 
 # Replace hard-coded interface values in enable_internet_forwarding.sh
 
 #TODO should test whether we can use wlan1 0 to forward traffic, of it it's busy monitoring.
-sed -i "5s/.*/INTERNET=$(sed '5q;d' $eth0)/" ./krackattacks-poc-zerokey/krackattack/enable_internet_forwarding.sh
+#sed -i 5s/.*/INTERNET=$(sed '5q;d' $eth0)/ ./krackattacks-poc-zerokey/krackattack/enable_internet_forwarding.sh
 
-sed -i "7s/.*/INTERNET=$(sed '7q;d' $wlan1)/" ./krackattacks-poc-zerokey/krackattack/enable_internet_forwarding.sh
+#sed -i 7s/.*/REPEATER=$(sed '7q;d' $wlan1)/ ./krackattacks-poc-zerokey/krackattack/enable_internet_forwarding.sh
 
 # Make modified hostapd instance. Only needs to be done once
 if [[ ! -x "./krackattacks-poc-zerokey/hostapd/hostapd" ]] 
@@ -30,23 +30,24 @@ then
 	cd ../../
 fi
 
-#Disable network
-sudo airmon-ng check kill
-
-# Disable hardware encryption, as bugs on some Wi-Fi network interface cards could interfere with the script used to check whether a client is vulnerable
-## NOTE we should also make sure that this is reversed when the user is done... Perhaps make it an option
 ./findVulnerable/krackattack/disable-hwcrypto.sh
 
-#Let user choose whether to reboot computer
+# Disable hardware encryption, as bugs on some Wi-Fi network interface cards could interfere with the script used to check whether a client is vulnerable
+
+
+## TODO NOTE we should also make sure that this is reversed when the user is done... Perhaps make it an option
+
+# TODO Let user choose whether to reboot computer
 ## NOTE not implemented
 
-#### NOTE: next sections not done
+#### TODO NOTE: next sections not done
 
-#RUN: systool -vm ath9k_htc
+#TODO RUN: systool -vm ath9k_htc
 
-## Customise files:
-
-
+#Disable network
+#sudo airmon-ng check kill
+sudo nmcli radio wifi off
+sudo rfkill unblock wifi
 
 
 
