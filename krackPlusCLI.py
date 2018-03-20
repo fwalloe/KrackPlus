26f051c9eac7a127188efe5010558e73b712b40e
Fredrik Walloe <code@fredrikwalloe.com>
2018-03-20 13:31:23 +0100

    Added general exception that will restore the internet connection if the program fails. However, this only works if the problem is in our script rather than in Vanhoef's. Perhaps modify his scripts so that they run our restore script

--- krackPlusCLI.py
+++ krackPlusCLI.py
@@ -73,7 +73,9 @@ def main():
             subprocess.call(["./restoreClientWifi.sh"])
             subprocess.call(["rm scanOutput.txt"], shell=True)
             log.info("PDF generated in '" + path + "'.")
-        
+        except:
+            log.info("Error occurred. Restoring wifi ...")
+            subprocess.call(["./restoreClientWifi.sh"])
     # Running attack scripts
     elif options.attack:
         try:
@@ -85,7 +87,9 @@ def main():
         except KeyboardInterrupt:
             log.info("Cleaning up and restoring wifi ...")
             subprocess.call(["./restoreClientWifi.sh"])
-
+	except:
+            log.info("Error occurred. Restoring wifi ...")
+            subprocess.call(["./restoreClientWifi.sh"])
 
 
     # Must specify an option    
