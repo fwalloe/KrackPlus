144bad9d113da86164a23307c1c90ecfa2834f89
Lars Magnus <lars.magnus.trinborgholen@gmail.com>
2018-03-20 14:32:56 +0100

    Imported parser.py instead of running it as a subprocess. This way we get more read-ability and can call functions.

--- parser.py
+++ parser.py
@@ -11,44 +11,55 @@
 
 import re	# used for regular expressions
 import time
-with open('./scanOutput.txt', 'r') as output:
-        i = 0
-	mac = ''
-	ip = ''
-        pairMacIP = {mac:ip}
-	# goes through the file line by line
-        while True:
-                time.sleep(1)
-                for line in output.readlines():
-                        if (str("]")) in line:
-                                line = line.split(']')[1]
-                                # Filter out interesting lines and parse them
-                        if (str("AP-STA-CONNECTED")) in line:
-                                connectedDevice = line.split("AP-STA-CONNECTED ")[1]
-                                print "Device connected: " + connectedDevice
-		        if (str("DHCP reply")) in line:
-                                mac = (line.split('DHCP')[0])
-			        mac = (str(mac).strip())[:-1]
-			        ip = line.split('reply')[1]
-			        ip = (ip.split('to')[0]).strip()
-			        pairMacIP.update({mac:ip})
-			        i = i+1
-		        elif str("vulnerable") in line:
-                                if str("DOESN'T") in line:
-                                        if str("group") in line:
-					        print (mac+" is not vulnerable to group key reinstallation")
-				        else:
-					        print (mac+" is not vulnerable to pairwise")  
-			        else:
-				        if str("group") in line:
-					        print (mac+" is vulnerable to group key reinstallation")
-				        else:
-					        print (mac+" is vulnerable to pairwise")  
-# Prints everything in the hashmap (must be expanded if we make it a hashmap with four values
-print "Connected devices:"
-for key, value in pairMacIP.iteritems():
-        if key != '' and value != '':
-                print "Mac: " + key + " has IP " + value
+def scanParser():
+	with open('./scanOutput.txt', 'r') as output:
+		i = 0
+		mac = ''
+		ip = ''
+		pairMacIP = {mac:ip}
+		# goes through the file line by line
+		while True:
+		        time.sleep(1)
+		        for line in output.readlines():
+		                if (str("]")) in line:
+		                        line = line.split(']')[1]
+		                        # Filter out interesting lines and parse them
+		                if (str("AP-STA-CONNECTED")) in line:
+		                        connectedDevice = line.split("AP-STA-CONNECTED ")[1]
+		                        print "Device connected: " + connectedDevice
+				if (str("DHCP reply")) in line:
+		                        mac = (line.split('DHCP')[0])
+					mac = (str(mac).strip())[:-1]
+					ip = line.split('reply')[1]
+					ip = (ip.split('to')[0]).strip()
+					pairMacIP.update({mac:ip})
+					i = i+1
+				elif str("vulnerable") in line:
+		                        if str("DOESN'T") in line:
+		                                if str("group") in line:
+							print (mac+" is not vulnerable to group key reinstallation")
+						else:
+							print (mac+" is not vulnerable to pairwise")  
+					else:
+						if str("group") in line:
+							print (mac+" is vulnerable to group key reinstallation")
+						else:
+							print (mac+" is vulnerable to pairwise")
+def getDevices():
+        return pairMacIP
+
+def nmap():
+        for key, value in pairMac.iteritems():
+                if key != '' and value != '':
+                        subprocess.call(["nmap -O " + value], shell=True)
+
+        
+def printConnectedDevices():
+	# Prints everything in the hashmap (must be expanded if we make it a hashmap with four values
+	print "Connected devices:"
+	for key, value in pairMacIP.iteritems():
+		if key != '' and value != '':
+		        print "Mac: " + key + " has IP " + value
 
 
        
