48dad91e03f419a3bc9450792a36e521226d2ac2
Fredrik Walløe <root@localhost.localdomain>
2018-04-12 10:07:01 +0200

    restoreNetwork: Now attempts to fix a range of interfaces if they exist

--- restoreClientWifi.sh
+++ restoreClientWifi.sh
@@ -7,16 +7,36 @@ systemctl start NetworkManager
 nmcli radio wifi off
 nmcli radio wifi on
 
-# Restore main interface by default name 
-ifconfig wlan0 down > /dev/null
-iwconfig wlan0 mode managed > /dev/null
-ifconfig wlan0 up > /dev/null
+# Restore intefaces by default names
+if (ifconfig -a | sed 's/[ \t].*//;/^$/d' | awk "FNR==3" | tr -d ':' | grep --quiet wlan0)
+then
+	ifconfig wlan0 down > /dev/null
+	iwconfig wlan0 mode managed > /dev/null
+	ifconfig wlan0 up > /dev/null
+fi 
 
-# Restore the external interface by default name 
-# TODO should check whether wlan1 exists before it runs
-#ifconfig wlan1 down > /dev/null
-#iwconfig wlan1 mode managed > /dev/null
-#ifconfig wlan1 up > /dev/null
+if (ifconfig -a | sed 's/[ \t].*//;/^$/d' | awk "FNR==3" | tr -d ':' | grep --quiet wlan0mon)
+then
+ 	ifconfig wlan0mon down > /dev/null
+fi
+
+if (ifconfig -a | sed 's/[ \t].*//;/^$/d' | awk "FNR==3" | tr -d ':' | grep --quiet wlan0sta1)
+then
+ 	ifconfig wlan0sta1 down > /dev/null
+fi
+
+if (ifconfig -a | sed 's/[ \t].*//;/^$/d' | awk "FNR==3" | tr -d ':' | grep --quiet wlan1sta1)
+then
+ 	ifconfig wlan1sta1 down > /dev/null
+fi
+
+if (ifconfig -a | sed 's/[ \t].*//;/^$/d' | awk "FNR==3" | tr -d ':' | grep --quiet wlan1)
+then
+	ifconfig wlan1 down > /dev/null
+	iwconfig wlan1 mode managed > /dev/null
+	ifconfig wlan1 up > /dev/null
+fi 
+
 
 # Loop over and try to restore interfaces by name
 for i in {1..3}
