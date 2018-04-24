21e3a1cc76bfc0cb6fc2a82845cc504db08ae93d
Fredrik Walløe <root@localhost.localdomain>
2018-04-24 11:00:50 +0200

    Made minor improvements

--- restoreClientWifi.sh
+++ restoreClientWifi.sh
@@ -15,28 +15,27 @@ then
 	ifconfig wlan0 up > /dev/null
 fi 
 
-if (ifconfig -a | sed 's/[ \t].*//;/^$/d' | awk "FNR==3" | tr -d ':' | grep --quiet wlan0mon)
-then
- 	ifconfig wlan0mon down > /dev/null
-fi
+#if (ifconfig -a | sed 's/[ \t].*//;/^$/d' | awk "FNR==3" | tr -d ':' | grep --quiet wlan0mon)
+#then
+# 	ifconfig wlan0mon down > /dev/null
+#fi
 
-if (ifconfig -a | sed 's/[ \t].*//;/^$/d' | awk "FNR==3" | tr -d ':' | grep --quiet wlan0sta1)
-then
- 	ifconfig wlan0sta1 down > /dev/null
-fi
+#if (ifconfig -a | sed 's/[ \t].*//;/^$/d' | awk "FNR==3" | tr -d ':' | grep --quiet wlan0sta1)
+#then
+# 	ifconfig wlan0sta1 down > /dev/null
+#fi
 
-if (ifconfig -a | sed 's/[ \t].*//;/^$/d' | awk "FNR==3" | tr -d ':' | grep --quiet wlan1sta1)
-then
- 	ifconfig wlan1sta1 down > /dev/null
-fi
+#if (ifconfig -a | sed 's/[ \t].*//;/^$/d' | awk "FNR==3" | tr -d ':' | grep --quiet wlan1sta1)
+#then
+# 	ifconfig wlan1sta1 down > /dev/null
+#fi
 
-if (ifconfig -a | sed 's/[ \t].*//;/^$/d' | awk "FNR==3" | tr -d ':' | grep --quiet wlan1)
-then
-	ifconfig wlan1 down > /dev/null
-	iwconfig wlan1 mode managed > /dev/null
-	ifconfig wlan1 up > /dev/null
-fi 
-
+#if (ifconfig -a | sed 's/[ \t].*//;/^$/d' | awk "FNR==3" | tr -d ':' | grep --quiet wlan1)
+#then
+#	ifconfig wlan1 down > /dev/null
+#	iwconfig wlan1 mode managed > /dev/null
+#	ifconfig wlan1 up > /dev/null
+#fi 
 
 # Loop over and try to restore interfaces by name
 for i in {1..3}
