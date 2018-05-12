1fe45f84bb1579e45a902a2722c589e2e9728636
Fredrik Walløe <root@localhost.localdomain>
2018-05-12 14:32:11 +0200

    Removed temporary file

--- TEMP-changeCredentials.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-SSID=$(sed '1q;d' networkCredentials.txt)
-password=$(sed '2q;d' networkCredentials.txt)
-
-sed -i '88s/.*/ssid=$SSID/' ./findVulnerable/hostapd/hostapd.conf
-sed -i '1146s/.*/wpa_passphrase=$password/' ./findVulnerable/hostapd/hostapd.conf
