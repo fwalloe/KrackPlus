fe27c177fc5e6e791c242ce0059c0dd0714670cf
Fredrik Walloe <code@fredrikwalloe.com>
2018-05-12 13:34:56 +0200

    restoreClientWifi: fixed a bug that caused this to not work.

--- restoreClientWifi.sh
+++ restoreClientWifi.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-#This script should (aggressively) restore wifi to wireles interfaces after either scan or attack. 
+##
+# restoreClientWifi.sh will (aggressively) restore wifi to wireless interfaces after either scan or attack runs. 
+##
 
-#TODO check if this helps
-#service network-manager restart
+sudo airmon-ng check kill > /dev/null
 
-sudo airmon-ng check kill >> /dev/null
+    ############# Restore intefaces by default names ################
 
-sudo service NetworkManager restart
-sudo service networking restart
-nmcli radio wifi off
-nmcli radio wifi on
-
-# Restore intefaces by default names
 if (ifconfig -a | sed 's/[ \t].*//;/^$/d' | awk "FNR==3" | tr -d ':' | grep --quiet wlan0)
 then
 	ifconfig wlan0 down > /dev/null
@@ -20,27 +15,34 @@ then
 	ifconfig wlan0 up > /dev/null
 fi 
 
-if (ifconfig -a | sed 's/[ \t].*//;/^$/d' | awk "FNR==3" | tr -d ':' | grep --quiet wlan0mon)
+if (ifconfig | sed 's/[ \t].*//;/^$/d' | awk "FNR==3" | tr -d ':' | grep --quiet wlan0mon)
 then
  	ifconfig wlan0mon down > /dev/null
 fi
 
-#if (ifconfig -a | sed 's/[ \t].*//;/^$/d' | awk "FNR==3" | tr -d ':' | grep --quiet wlan0sta1)
-#then
-# 	ifconfig wlan0sta1 down > /dev/null
-#fi
+if (ifconfig | sed 's/[ \t].*//;/^$/d' | awk "FNR==3" | tr -d ':' | grep --quiet wlan0sta1)
+then
+ 	ifconfig wlan0sta1 down > /dev/null
+fi
 
-#if (ifconfig -a | sed 's/[ \t].*//;/^$/d' | awk "FNR==3" | tr -d ':' | grep --quiet wlan1sta1)
-#then
-# 	ifconfig wlan1sta1 down > /dev/null
-#fi
+if (ifconfig | sed 's/[ \t].*//;/^$/d' | awk "FNR==4" | tr -d ':' | grep --quiet wlan1sta1)
+then
+ 	ifconfig wlan1sta1 down > /dev/null
+fi
 
-#if (ifconfig -a | sed 's/[ \t].*//;/^$/d' | awk "FNR==3" | tr -d ':' | grep --quiet wlan1)
-#then
-#	ifconfig wlan1 down > /dev/null
-#	iwconfig wlan1 mode managed > /dev/null
-#	ifconfig wlan1 up > /dev/null
-#fi 
+if (ifconfig -a | sed 's/[ \t].*//;/^$/d' | awk "FNR==4" | tr -d ':' | grep --quiet wlan1)
+then
+	ifconfig wlan1 down > /dev/null
+	iwconfig wlan1 mode managed > /dev/null
+	ifconfig wlan1 up > /dev/null
+fi 
+
+    ############# Attempt to restore connection ################
+
+sudo service NetworkManager restart > /dev/null
+sudo service networking restart > /dev/null
+nmcli radio wifi off > /dev/null
+nmcli radio wifi on > /dev/null
 
 # Loop over and try to restore interfaces by name
 #for i in {1..3}
